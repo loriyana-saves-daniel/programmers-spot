be1e7f64f439a11774730c0026f9a40b35170dbd
Daniel Isov <danielisov96@gmail.com>
2017-02-11 03:40:48 +0200

    Fix

---
+++ ProgrammersSpot/Business/ProgrammersSpot.Business.Data/Contracts/IProgrammersSpotDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace ProgrammersSpot.Business.Data.Contracts
+{
+    public interface IProgrammersSpotDbContext : IDisposable
+    {
+        IDbSet<T> Set<T>() where T : class;
+
+        DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+
+        void SaveChanges();
+    }
+}
